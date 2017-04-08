714839d87ddc853956bf67395c16d92e5ad3fb78
EC2 Default User <ec2-user@ip-172-31-14-187.ec2.internal>
2017-04-08 16:57:05 +0000

    json structure

--- api.py
+++ api.py
@@ -20,6 +20,9 @@ def __get_disk_usage():
     total, used, free, percent = psutil.disk_usage('/')
     return {"total": total, "free": free, "used": used, "percent": percent}
 
+def __get_mem_info():
+    total, available, percent, used, free, active, inactive, buffers, cached, shared = psutil.virtual_memory()
+    return {"total": total, "available": available, "percent": percent, "used": used, "free": free, "active": active, "inactive": inactive, "buffers": buffers, "cached": cached, "shared": shared}
 
 def __getloadavg():
     (min1,min5,min10) = os.getloadavg()
@@ -29,14 +32,23 @@ def __getloadavg():
 def __get_cpu_count():
     return {"num_cpu": psutil.cpu_count()}
 
+def __get_net_io_counters():
+    bytes_sent, bytes_recv, packets_sent, packets_recv, errin, errout, dropin, dropout = psutil.net_io_counters()
+    return {"bytes_sent": bytes_sent, "bytes_recv": bytes_recv, "packets_sent": packets_sent, "packets_recv": packets_recv, "errin": errin, "errout": errout, "dropin": dropin, "dropout": dropout}
+
 
 def getAllInfo():
     return {
         "load_avg": __getloadavg(),
         "get_cpu_count": __get_cpu_count(),
+        "get_mem_info": __get_mem_info(),
         "disk_usage": __get_disk_usage(),
+        "get_net_io_counters": __get_net_io_counters(),
         "special_processes": __get_info_about_named_processes(),
         "metadata": __get_meta_data()
     }
+
+    
+
 if __name__ == "__main__":
     print(getAllInfo())
---
+++ eventloop.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import time
+import requests
+
+import subprocess
+
+import pprint
+
+from api import getAllInfo
+from sar import parser
+
+
+def get_data():
+    p = subprocess.Popen('sar -f /var/log/sa/sar08'.split(' '), shell=True, stdout=subprocess.PIPE)
+    outs, errs = p.communicate()
+    
+    
+    sfile = ('%s/%s' % ('/var/log/sa', 'sar08'))
+    insar = parser.Parser(sfile)
+    #pprint.pprint(insar.get_sar_info())
+
+
+
+    allinfo = getAllInfo()
+    pprint.pprint(allinfo['get_mem_info'])
+
+    meta = allinfo['metadata']
+    meta['num_cpu'] = allinfo['get_cpu_count']
+
+    cpu = {}
+    cpu['load_avg'] = allinfo['load_avg']
+
+    mem = allinfo['get_mem_info']
+    
+    storage = allinfo['disk_usage']
+
+    network = allinfo['get_net_io_counters']
+
+    processes = allinfo['special_processes']
+
+    data = {"meta": meta, "cpu": cpu, "mem": mem, "storage": storage, "network": network, "processes": processes}
+    return data
+
+
+while True:
+    url = "https://stackcents.herokuapp.com/echo/"
+    data = get_data()
+    pprint.pprint(data)
+    r = requests.post(url, data=data)
+    print(r.status_code)
+    #print(r.json())
+    time.sleep(10)
+    
