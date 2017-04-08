8ec6bcee9108691bd361205c8b660cfa7cdfb2c0
EC2 Default User <ec2-user@ip-172-31-14-187.ec2.internal>
2017-04-08 19:48:11 +0000

    history loadavg

--- eventloop.py
+++ eventloop.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import time
+import datetime
 import requests
-
 import subprocess
 
 import pprint
@@ -12,14 +12,20 @@ from sar import parser
 
 
 def get_data():
-    p = subprocess.Popen('sar -f /var/log/sa/sar08'.split(' '), shell=True, stdout=subprocess.PIPE)
+    p = subprocess.Popen(["sar -q"], shell=True, stdout=subprocess.PIPE)
     outs, errs = p.communicate()
-    
-    
-    sfile = ('%s/%s' % ('/var/log/sa', 'sar08'))
-    insar = parser.Parser(sfile)
-    #pprint.pprint(insar.get_sar_info())
 
+    cpu = []
+    index = 0
+    ignore = 3
+    for line in outs.split('\n'):
+        if ignore > 0:
+            ignore -= 1
+        else:
+            parse = line.split()
+            if len(parse) == 7:
+                cpu.append({"index": index, "timestamp": "%s %s"%(parse[0],parse[1]), "load_avg_1": parse[4], "load_avg_5": parse[5], "load_avg_15": parse[6]})
+                index += 1
 
 
     allinfo = getAllInfo()
@@ -28,8 +34,8 @@ def get_data():
     meta = allinfo['metadata']
     meta['num_cpu'] = allinfo['get_cpu_count']
 
-    cpu = {}
-    cpu['load_avg'] = allinfo['load_avg']
+    #cpu = {}
+    #cpu['load_avg'] = allinfo['load_avg']
 
     mem = allinfo['get_mem_info']
     
@@ -39,14 +45,14 @@ def get_data():
 
     processes = allinfo['special_processes']
 
-    data = {"id": meta['instance-id'], "meta": meta, "cpu": cpu, "mem": mem, "storage": storage, "network": network, "processes": processes}
+    data = {"id": meta['instance-id'], "timestamp": time.time(), "meta": meta, "cpu": cpu, "mem": mem, "storage": storage, "network": network, "processes": processes}
     return data
 
 
 while True:
     url = "https://stackcents.herokuapp.com/echo/"
     data = get_data()
-    #pprint.pprint(data)
+    pprint.pprint(data)
     r = requests.post(url, data=data)
     print(r.status_code)
     #print(r.json())
