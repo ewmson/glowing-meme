b95d7ef29fb41b8f92a5ebf39e62d46583516803
EC2 Default User <ec2-user@ip-172-31-14-187.ec2.internal>
2017-04-09 03:21:00 +0000

    hypehype

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ yum -y install git
 git clone https://github.com/ewmson/glowing-meme.git
 cd glowing-meme
 yum -y install gcc
+yum install sysstat
+systemctl enable sysstat
 pip install -r requirements.txt
 pip install --upgrade requests
 nohup python eventloop.py </dev/null > hype.log 2>&1 &
